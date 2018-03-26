09e34bbb824231b1ecb76a5bb62bf766bbc54f99
Jason Dias <dias.jason9@gmail.com>
2018-03-26 11:05:30 -0400

    added path

--- semisup/tools/data_dirs.py
+++ semisup/tools/data_dirs.py
@@ -1,18 +1,1 @@
-"""
-This file contains the directory paths to data sets on your hard drive.
-Change the directories and .gitignore this file.
-"""
-
-synth = '/work/haeusser/data/synth/'
-stl10 = '/work/haeusser/data/stl10_binary/'
-svhn =  '/work/haeusser/data/svhn/'
-
-imagenet = '/work/haeusser/data/imagenet/raw-data/'
-imagenet_labels = '/usr/wiss/haeusser/libs/tfmodels/inception/inception/data/imagenet_lsvrc_2015_synsets.txt'
-gtsrb = '/work/haeusser/data/gtsrb/'
-usps = '/work/haeusser/data/usps/'
-office = '/work/haeusser/data/office/'
-mnistm = ''
-
-mnist = '/Users/jasondias/Desktop/457/learning_by_association/mnist/'
-nhltb = '/Users/jasondias/Desktop/457/event_detection/DynamicImageLibrary/'
+nhltb = '/content/drive/DynamicImageLibrary/'
